e2887f49d11e95b62db4253b1ea27e5ac01b6026
anjalstha <lostnjl@gmail.com>
2022-12-14 17:05:06 +0545

    3

--- lib/description_page.dart
+++ lib/description_page.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+String baconText =
+    'Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together. Identify your goals Before you get started on becoming rich, devise a financial plan. Here are a few questions you may ask yourself as you put your plan together.';
 
 class DescriptionPage extends StatelessWidget {
-  const DescriptionPage({super.key});
+  const DescriptionPage({
+    super.key,
+    required this.title,
+    required this.imagePath,
+  });
+
+  final String title;
+  final String imagePath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('This is How You get Rich'),
+        title: Text(title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Image.asset(imagePath),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                baconText,
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,82 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:howto/description_page.dart';
+import 'package:howto/main.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color backgroundColor = Colors.lightGreen;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundColor,
       appBar: AppBar(
-        title: const Center(child: Text('Hey Sathi')),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                if (backgroundColor == Colors.lightGreen) {
+                  backgroundColor = Colors.white;
+                } else {
+                  backgroundColor = Colors.lightGreen;
+                }
+              });
+            },
+            icon: const Icon(
+              Icons.color_lens,
+            ),
+          ),
+        ],
+        title: const Text('Hey Sathi'),
+      ),
+      drawer: SafeArea(
+        child: Drawer(
+          child: Column(
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+                child: ListTile(
+                  title: Text(
+                    'Kya karna hain bhai',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                onTap: () {},
+                title: const Text(
+                  'Settings',
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const LoginPage();
+                      },
+                    ),
+                  );
+                },
+                title: const Text(
+                  'LogOut',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -63,12 +128,18 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+
+            //1st one ---------------------------------------------------
+
             GestureDetector(
               onTap: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (BuildContext context) {
-                      return const DescriptionPage();
+                      return const DescriptionPage(
+                        title: 'How to get rich',
+                        imagePath: 'images/love.png',
+                      );
                     },
                   ),
                 );
@@ -104,64 +175,142 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 10.0,
-                vertical: 5.0,
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      spreadRadius: 2,
-                      blurRadius: 2,
-                      offset: Offset(
-                        2,
-                        2,
-                      ),
-                    )
-                  ]),
-              child: Column(
-                children: [
-                  Image.asset('images/love.png'),
-                  const ListTile(
-                    title: Text('How to get rich'),
-                    trailing: Icon(Icons.arrow_forward_ios),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const DescriptionPage(
+                        title: 'How to not get bankrupt',
+                        imagePath:
+                            'https://freepngimg.com/thumb/kanye_west/155284-kanye-photos-west-free-download-png-hd.png',
+                      );
+                    },
                   ),
-                ],
+                );
+              },
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5.0,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        spreadRadius: 2,
+                        blurRadius: 2,
+                        offset: Offset(
+                          2,
+                          2,
+                        ),
+                      )
+                    ]),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://freepngimg.com/thumb/kanye_west/155284-kanye-photos-west-free-download-png-hd.png'),
+                    const ListTile(
+                      title: Text('How to not get bankrupt'),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Container(
-              width: double.infinity,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 10.0,
-                vertical: 5.0,
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      spreadRadius: 2,
-                      blurRadius: 2,
-                      offset: Offset(
-                        2,
-                        2,
-                      ),
-                    )
-                  ]),
-              child: Column(
-                children: [
-                  Image.asset('images/love.png'),
-                  const ListTile(
-                    title: Text('How to get rich'),
-                    trailing: Icon(Icons.arrow_forward_ios),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const DescriptionPage(
+                        title: 'How to not be poor',
+                        imagePath:
+                            'https://png.pngtree.com/background/20210712/original/pngtree-set-of-idul-adha-help-with-meat-packages-to-poor-people-picture-image_1183417.jpg',
+                      );
+                    },
                   ),
-                ],
+                );
+              },
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5.0,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        spreadRadius: 2,
+                        blurRadius: 2,
+                        offset: Offset(
+                          2,
+                          2,
+                        ),
+                      )
+                    ]),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://png.pngtree.com/background/20210712/original/pngtree-set-of-idul-adha-help-with-meat-packages-to-poor-people-picture-image_1183417.jpg'),
+                    const ListTile(
+                      title: Text('How to not be poor'),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const DescriptionPage(
+                        title: 'How to earn money',
+                        imagePath:
+                            'https://png.pngtree.com/background/20210706/original/pngtree-money-background-picture-image_200823.jpg',
+                      );
+                    },
+                  ),
+                );
+              },
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5.0,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        spreadRadius: 2,
+                        blurRadius: 2,
+                        offset: Offset(
+                          2,
+                          2,
+                        ),
+                      )
+                    ]),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://png.pngtree.com/background/20210706/original/pngtree-money-background-picture-image_200823.jpg'),
+                    const ListTile(
+                      title: Text('How to earn money'),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
